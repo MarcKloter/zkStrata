6f368648c838818d80690896bd7ea90704684c58
Marc <marc.kloter@bluewin.ch>
2019-10-09 20:35:55 +0200

    Add dynamic error message for multiline errors.

--- src/main/java/zkstrata/exceptions/CompilerException.java
+++ src/main/java/zkstrata/exceptions/CompilerException.java
@@ -2,9 +2,9 @@ package zkstrata.exceptions;
 
 import zkstrata.utils.ErrorUtils;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // TODO: consider error codes
 public class CompilerException extends RuntimeException {
@@ -15,13 +15,20 @@ public class CompilerException extends RuntimeException {
     }
 
     public CompilerException(String message, String statement, Position position) {
-        super(String.format("Error at line %s: %s%n%s", position.getLine(), message, ErrorUtils.underline(statement, position)));
-        this.errors = List.of(position);
+        this(message, statement, List.of(position));
     }
 
     public CompilerException(String message, String statement, List<Position> positions) {
-        super(String.format("Error: %s%n%s", message, ErrorUtils.underline(statement, positions)));
+        super(String.format("%s: %s%n%s", createMessage(positions), message, ErrorUtils.underline(statement, positions)));
         this.errors = positions;
+    }
+
+    private static String createMessage(List<Position> positions) {
+        Set<Integer> lineNumbers = positions.stream().map(Position::getLine).collect(Collectors.toSet());
+        if (lineNumbers.size() == 1)
+            return String.format("Error at line %s", lineNumbers.iterator().next());
+        else
+            return String.format("Error at lines %s", lineNumbers.stream().map(Object::toString).collect(Collectors.joining(", ")));
     }
 
     public List<Position> getErrors() {
