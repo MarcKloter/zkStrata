26a5bc0eb0b792d200fc524d40b96daf8dcce3a4
Marc <marc.kloter@bluewin.ch>
2019-10-16 16:39:36 +0200

    Add annotations for implications and contradictions.

---
+++ src/main/java/zkstrata/analysis/Contradiction.java
@@ -0,0 +1,14 @@
+package zkstrata.analysis;
+
+import zkstrata.domain.gadgets.Gadget;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Contradiction {
+    Class<? extends Gadget>[] propositions();
+}
