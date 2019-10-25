34261e550153c770787f7fcb82fc4b78f67cd0eb
Marc Kloter <marc.kloter@students.fhnw.ch>
2019-10-25 09:16:50 +0200

    Add additional unit tests for EqualityGadget implementation

--- src/test/java/gadgets/EqualityGadgetTest.java
+++ src/test/java/gadgets/EqualityGadgetTest.java
@@ -6,6 +6,7 @@ import zkstrata.domain.data.types.Literal;
 import zkstrata.domain.data.types.Reference;
 import zkstrata.domain.data.types.wrapper.InstanceVariable;
 import zkstrata.domain.data.types.wrapper.WitnessVariable;
+import zkstrata.domain.gadgets.Gadget;
 import zkstrata.domain.gadgets.impl.EqualityGadget;
 import zkstrata.exceptions.CompileTimeException;
 import zkstrata.exceptions.Position;
@@ -13,6 +14,7 @@ import zkstrata.exceptions.Position;
 import java.math.BigInteger;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,8 +24,8 @@ public class EqualityGadgetTest {
     private static final InstanceVariable INSTANCE_VAR_17 = new InstanceVariable(new Literal(BigInteger.valueOf(17)), DUMMY_POS);
     private static final InstanceVariable INSTANCE_VAR_41 = new InstanceVariable(new Literal(BigInteger.valueOf(41)), DUMMY_POS);
 
-    private static final Reference REF_1 = new Reference(BigInteger.class, "alias", new Selector(List.of("selector")));
-    private static final Reference REF_2 = new Reference(BigInteger.class, "alias", new Selector(List.of("selector")));
+    private static final Reference REF_1 = new Reference(BigInteger.class, "alias1", new Selector(List.of("selector1")));
+    private static final Reference REF_2 = new Reference(BigInteger.class, "alias2", new Selector(List.of("selector2")));
     private static final WitnessVariable WITNESS_VAR_1 = new WitnessVariable(REF_1, REF_1, DUMMY_POS);
     private static final WitnessVariable WITNESS_VAR_2 = new WitnessVariable(REF_2, REF_2, DUMMY_POS);
 
@@ -36,14 +38,53 @@ public class EqualityGadgetTest {
     }
 
     @Test
+    void Instance_Equals_Instance_No_Contradiction() {
+        EqualityGadget equalityGadget = new EqualityGadget(INSTANCE_VAR_41, INSTANCE_VAR_41);
+        assertDoesNotThrow(() -> {
+            EqualityGadget.checkContradiction(equalityGadget);
+        });
+    }
+
+    @Test
     void Witness_Equals_Self_Substitution() {
         EqualityGadget equalityGadget = new EqualityGadget(WITNESS_VAR_1, WITNESS_VAR_1);
         assertEquals(Collections.emptyList(), EqualityGadget.removeWitnessEqualsSelf(equalityGadget));
     }
 
     @Test
+    void Witness_Equals_Self_No_Substitution() {
+        EqualityGadget equalityGadget = new EqualityGadget(WITNESS_VAR_1, WITNESS_VAR_2);
+        assertEquals(List.of(equalityGadget), EqualityGadget.removeWitnessEqualsSelf(equalityGadget));
+    }
+
+    @Test
     void Instance_Equals_Instance_Substitution() {
         EqualityGadget equalityGadget = new EqualityGadget(INSTANCE_VAR_17, INSTANCE_VAR_17);
         assertEquals(Collections.emptyList(), EqualityGadget.removeInstanceEqualsInstance(equalityGadget));
+    }
+
+    @Test
+    void Instance_Equals_Instance_No_Substitution() {
+        EqualityGadget equalityGadget = new EqualityGadget(INSTANCE_VAR_17, INSTANCE_VAR_41);
+        assertEquals(List.of(equalityGadget), EqualityGadget.removeInstanceEqualsInstance(equalityGadget));
+    }
+
+    @Test
+    void Is_Equal_To() {
+        EqualityGadget equalityGadget1 = new EqualityGadget(WITNESS_VAR_1, WITNESS_VAR_2);
+        EqualityGadget equalityGadget2 = new EqualityGadget(WITNESS_VAR_2, WITNESS_VAR_1);
+        assertTrue(equalityGadget1.isEqualTo(equalityGadget2));
+    }
+
+    @Test
+    void Parity_Implication() {
+        EqualityGadget equalityGadget1 = new EqualityGadget(WITNESS_VAR_1, INSTANCE_VAR_17);
+        EqualityGadget equalityGadget2 = new EqualityGadget(WITNESS_VAR_2, WITNESS_VAR_1);
+        EqualityGadget implication = new EqualityGadget(INSTANCE_VAR_17, WITNESS_VAR_2);
+
+        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget1, equalityGadget2);
+        assertTrue(result.isPresent());
+        assertTrue(result.get() instanceof EqualityGadget);
+        assertTrue(implication.isEqualTo((EqualityGadget) result.get()));
     }
 }
