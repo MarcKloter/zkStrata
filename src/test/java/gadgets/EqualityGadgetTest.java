94aca76d5142bff6b2108f4f34d8ce6a8881f2cd
Marc <marc.kloter@bluewin.ch>
2019-11-07 20:36:13 +0100

    Add unit test to equality gadget

--- src/test/java/gadgets/EqualityGadgetTest.java
+++ src/test/java/gadgets/EqualityGadgetTest.java
@@ -95,7 +95,7 @@ public class EqualityGadgetTest {
         EqualityGadget equalityGadget2 = new EqualityGadget(WITNESS_VAR_2, WITNESS_VAR_1);
         EqualityGadget implication = new EqualityGadget(INSTANCE_VAR_17, WITNESS_VAR_2);
 
-        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget2, equalityGadget1);
+        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget1, equalityGadget2);
         assertTrue(result.isPresent());
         assertTrue(result.get() instanceof EqualityGadget);
         assertTrue(implication.isEqualTo((EqualityGadget) result.get()));
@@ -107,7 +107,19 @@ public class EqualityGadgetTest {
         EqualityGadget equalityGadget2 = new EqualityGadget(WITNESS_VAR_1, WITNESS_VAR_2);
         EqualityGadget implication = new EqualityGadget(INSTANCE_VAR_17, WITNESS_VAR_2);
 
-        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget2, equalityGadget1);
+        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget1, equalityGadget2);
+        assertTrue(result.isPresent());
+        assertTrue(result.get() instanceof EqualityGadget);
+        assertTrue(implication.isEqualTo((EqualityGadget) result.get()));
+    }
+
+    @Test
+    void Parity_Implication_4() {
+        EqualityGadget equalityGadget1 = new EqualityGadget(INSTANCE_VAR_17, WITNESS_VAR_1);
+        EqualityGadget equalityGadget2 = new EqualityGadget(WITNESS_VAR_1, WITNESS_VAR_2);
+        EqualityGadget implication = new EqualityGadget(INSTANCE_VAR_17, WITNESS_VAR_2);
+
+        Optional<Gadget> result = EqualityGadget.implyEquality(equalityGadget1, equalityGadget2);
         assertTrue(result.isPresent());
         assertTrue(result.get() instanceof EqualityGadget);
         assertTrue(implication.isEqualTo((EqualityGadget) result.get()));
