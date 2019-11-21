9c874c19ac97a8a456ef5442ecf6035b9efc308d
Marc <marc.kloter@bluewin.ch>
2019-11-21 22:08:50 +0100

    Add comment to @Implication

--- src/main/java/zkstrata/analysis/Implication.java
+++ src/main/java/zkstrata/analysis/Implication.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a method as implication rule.
+ *
+ * An implication rule takes instances of {@link Gadget} as defined in the property {@code assumption} as arguments and
+ * returns an {@link java.util.Optional} of {@link Gadget} which can be implied from the {@code assumption}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Implication {
