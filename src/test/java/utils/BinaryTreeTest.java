ea35e4f390175c916751ab0085023ae1449301bf
Marc <marc.kloter@bluewin.ch>
2019-12-05 20:14:41 +0100

    Refactor binary tree unit tests

--- src/test/java/utils/BinaryTreeTest.java
+++ src/test/java/utils/BinaryTreeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import zkstrata.utils.BinaryTree;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static zkstrata.utils.BinaryTree.Node;
 
 public class BinaryTreeTest {
     private static final BinaryTree.Node<String> LEAF_1 = new BinaryTree.Node<>("One");
@@ -11,42 +12,58 @@ public class BinaryTreeTest {
     private static final BinaryTree.Node<String> LEAF_3 = new BinaryTree.Node<>("Three");
     private static final BinaryTree.Node<String> LEAF_4 = new BinaryTree.Node<>("Four");
 
-    private static final BinaryTree<String> TREE_1A = new BinaryTree<>(new BinaryTree.Node<>(new BinaryTree.Node<>(LEAF_1, LEAF_4), new BinaryTree.Node<>(LEAF_3, LEAF_2)));
-    private static final BinaryTree<String> TREE_1B = new BinaryTree<>(new BinaryTree.Node<>(new BinaryTree.Node<>(LEAF_1, LEAF_4), new BinaryTree.Node<>(LEAF_3, LEAF_2)));
-    private static final BinaryTree<String> TREE_2A = new BinaryTree<>(new BinaryTree.Node<>(LEAF_1, new BinaryTree.Node<>(LEAF_3, LEAF_2)));
-    private static final BinaryTree<String> TREE_2B = new BinaryTree<>(new BinaryTree.Node<>(LEAF_1, new BinaryTree.Node<>(LEAF_3, LEAF_2)));
-    private static final BinaryTree<String> TREE_3A = new BinaryTree<>(new BinaryTree.Node<>(new BinaryTree.Node<>(LEAF_1, LEAF_4), LEAF_2));
-    private static final BinaryTree<String> TREE_3B = new BinaryTree<>(new BinaryTree.Node<>(new BinaryTree.Node<>(null, LEAF_4), LEAF_2));
-    private static final BinaryTree<String> TREE_4A = new BinaryTree<>(new BinaryTree.Node<>(LEAF_1, LEAF_4));
-    private static final BinaryTree<String> TREE_4B = new BinaryTree<>(new BinaryTree.Node<>(LEAF_1, null));
-
     @Test
-    void Equals_Null() {
-        assertNotEquals(TREE_1A, null);
-        assertNotEquals(TREE_2A, null);
+    void Not_Equals_Null() {
+        BinaryTree<String> tree = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        assertNotEquals(null, tree);
     }
 
     @Test
-    void Equals_Other_Object() {
-        assertNotEquals(TREE_1A, "String");
-        assertNotEquals(TREE_2A, 1234);
+    void Not_Equals_Other_Object() {
+        BinaryTree<String> tree = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        assertNotEquals("String", tree);
+        assertNotEquals(1234, tree);
     }
 
     @Test
-    void Equals() {
-        assertEquals(TREE_1A, TREE_1B);
-        assertEquals(TREE_2A, TREE_2B);
+    void Equals_1() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        assertEquals(treeA, treeB);
     }
 
     @Test
-    void Not_Equals() {
-        assertNotEquals(TREE_3A, TREE_3B);
-        assertNotEquals(TREE_4A, TREE_4B);
+    void Equals_2() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(LEAF_1, new Node<>(LEAF_3, LEAF_2)));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(LEAF_1, new Node<>(LEAF_3, LEAF_2)));
+        assertEquals(treeA, treeB);
     }
 
     @Test
-    void HashCode() {
-        assertEquals(TREE_1A.hashCode(), TREE_1B.hashCode());
-        assertEquals(TREE_2A.hashCode(), TREE_2B.hashCode());
+    void Not_Equals_1() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), LEAF_2));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(new Node<>(null, LEAF_4), LEAF_2));
+        assertNotEquals(treeA, treeB);
+    }
+
+    @Test
+    void Not_Equals_2() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(LEAF_1, LEAF_4));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(LEAF_1, null));
+        assertNotEquals(treeA, treeB);
+    }
+
+    @Test
+    void HashCode_1() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(new Node<>(LEAF_1, LEAF_4), new Node<>(LEAF_3, LEAF_2)));
+        assertEquals(treeA.hashCode(), treeB.hashCode());
+    }
+
+    @Test
+    void HashCode_2() {
+        BinaryTree<String> treeA = new BinaryTree<>(new Node<>(LEAF_1, new Node<>(LEAF_3, LEAF_2)));
+        BinaryTree<String> treeB = new BinaryTree<>(new Node<>(LEAF_1, new Node<>(LEAF_3, LEAF_2)));
+        assertEquals(treeA.hashCode(), treeB.hashCode());
     }
 }
