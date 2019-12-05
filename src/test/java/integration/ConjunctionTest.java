0f0c8b7524fd82a843f2074995fa03ee25d8fcd5
Marc <marc.kloter@bluewin.ch>
2019-12-05 21:06:40 +0100

    Add unit tests for conjunctions

---
+++ src/test/java/integration/ConjunctionTest.java
@@ -0,0 +1,57 @@
+package integration;
+
+import org.junit.jupiter.api.Test;
+import zkstrata.compiler.Arguments;
+import zkstrata.compiler.Compiler;
+import zkstrata.exceptions.CompileTimeException;
+import zkstrata.utils.ArgumentsBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConjunctionTest {
+    @Test
+    void Or_Conjunction_Should_Succeed() {
+        assertDoesNotThrow(() -> {
+            Arguments args = new ArgumentsBuilder(ConjunctionTest.class)
+                    .withStatement("or_conjunction")
+                    .withInstance("pass", "passport.metadata")
+                    .build();
+            Compiler.run(args);
+        });
+    }
+
+    @Test
+    void Or_Conjunction_Contradiction_Should_Succeed() {
+        CompileTimeException exception = assertThrows(CompileTimeException.class, () -> {
+            Arguments args = new ArgumentsBuilder(ConjunctionTest.class)
+                    .withStatement("or_conjunction_contradiction")
+                    .withInstance("pass", "passport.metadata")
+                    .build();
+            Compiler.run(args);
+        });
+        assertTrue(exception.getMessage().toLowerCase().contains("contradiction"));
+    }
+
+    @Test
+    void And_Conjunction_Should_Succeed() {
+        assertDoesNotThrow(() -> {
+            Arguments args = new ArgumentsBuilder(ConjunctionTest.class)
+                    .withStatement("and_conjunction")
+                    .withInstance("pass", "passport.metadata")
+                    .build();
+            Compiler.run(args);
+        });
+    }
+
+    @Test
+    void And_Conjunction_Contradiction_Should_Succeed() {
+        CompileTimeException exception = assertThrows(CompileTimeException.class, () -> {
+            Arguments args = new ArgumentsBuilder(ConjunctionTest.class)
+                    .withStatement("and_conjunction_contradiction")
+                    .withInstance("pass", "passport.metadata")
+                    .build();
+            Compiler.run(args);
+        });
+        assertTrue(exception.getMessage().toLowerCase().contains("contradiction"));
+    }
+}
