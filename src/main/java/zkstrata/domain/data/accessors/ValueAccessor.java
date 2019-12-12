6b2ec4748b9ea56092baf8f7e75ed47f24702670
Marc <marc.kloter@bluewin.ch>
2019-12-12 10:38:22 +0100

    Add comments to ValueAccessor interface

--- src/main/java/zkstrata/domain/data/accessors/ValueAccessor.java
+++ src/main/java/zkstrata/domain/data/accessors/ValueAccessor.java
@@ -4,7 +4,18 @@ import zkstrata.domain.data.Selector;
 import zkstrata.domain.data.types.Value;
 
 public interface ValueAccessor {
+    /**
+     * Returns the {@link Value} associated with the provided {@code selector}.
+     *
+     * @param selector {@link Selector} to access a value for
+     * @return {@link Value} associated with {@code selector} or {@code null} if nothing is associated with it
+     */
     Value getValue(Selector selector);
 
+    /**
+     * Returns the information of origin of the values of this accessor as string (e.g. a filename).
+     *
+     * @return information of origin of the values accessible by this accessor
+     */
     String getSource();
 }
