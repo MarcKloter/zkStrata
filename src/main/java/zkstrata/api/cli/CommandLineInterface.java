e16c769938677a1008bb03c00dc65202c0d73cbc
Marc <marc.kloter@bluewin.ch>
2019-12-13 14:13:04 +0100

    Refactor CLI to enable testing

--- src/main/java/zkstrata/api/cli/CommandLineInterface.java
+++ src/main/java/zkstrata/api/cli/CommandLineInterface.java
@@ -12,41 +12,69 @@ import zkstrata.domain.data.schemas.Schema;
 import zkstrata.domain.data.schemas.dynamic.JsonSchema;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import static zkstrata.compiler.Arguments.Statement;
 
 public class CommandLineInterface {
     private Options options;
+    private PrintWriter printWriter;
 
-    public CommandLineInterface() {
+    public CommandLineInterface(PrintWriter printWriter) {
+        this.printWriter = printWriter;
         this.options = new OptionBuilder().withLongOpts().withFlags().build();
     }
 
     /**
      * Parses the supplied arguments using Apache Commons CLI.
      *
-     * @param args Command line arguments.
-     * @return Parsed arguments for the zkStrata compiler to use.
+     * @param args command line arguments
+     * @return Parsed arguments for the zkStrata compiler to use
+     * @throws SpecialOptionException if a special flag matched, exiting the program early
      */
     public Arguments parse(String[] args) {
         try {
+            checkSpecialFlags(args);
             return parseArguments(args);
         } catch (ParseException e) {
-            printHelp();
+            printHelp(printWriter);
+            throw new SpecialOptionException();
+        } finally {
+            printWriter.flush();
         }
-
-        throw new IllegalStateException();
     }
 
+    /**
+     * Checks whether special flags were used, independent of whether the {@code args} are valid.
+     *
+     * @param args command line arguments
+     * @throws ParseException         if the arguments do not contain anything recognizable
+     * @throws SpecialOptionException if a special flag matched, exiting the program early
+     */
+    private void checkSpecialFlags(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        Options flagOptions = new OptionBuilder().withFlags().build();
+        CommandLine cmd = parser.parse(flagOptions, args, true);
+        checkFlags(cmd);
+    }
+
+    /**
+     * Parses the given {@code args} according to the {@link CommandLineInterface#options}.
+     *
+     * @param args command line arguments
+     * @return {@link Arguments} representation of the parsed {@code args}
+     * @throws ParseException         if the provided {@code args} are malformed
+     * @throws SpecialOptionException if a special flag matched, exiting the program early
+     */
     private Arguments parseArguments(String[] args) throws ParseException {
         CommandLineParser parser = new DefaultParser();
-        checkFlags(parser.parse(new OptionBuilder().withFlags().build(), args, true));
         CommandLine cmd = parser.parse(options, args);
         checkFlags(cmd);
 
@@ -54,6 +82,7 @@ public class CommandLineInterface {
         String name = getStatementName(file);
         Statement statement = new Statement(file, getStatement(file));
         List<Statement> premises = getPremises(cmd);
+        setVerbosity(cmd);
 
         HashMap<String, ValueAccessor> witnessFiles = getWitnessData(cmd);
         HashMap<String, Schema> schemaFiles = getSchemas(cmd);
@@ -66,43 +95,51 @@ public class CommandLineInterface {
      * Checks whether special option flags were used.
      *
      * @param cmd {@link CommandLine} object that represents a list of arguments
+     * @throws SpecialOptionException if a special flag matched, exiting the program early
      */
     private void checkFlags(CommandLine cmd) {
-        if (cmd.hasOption("help"))
-            printHelp();
+        if (cmd.hasOption("help")) {
+            printHelp(printWriter);
+            throw new SpecialOptionException();
+        }
 
-        if (cmd.hasOption("version"))
-            printVersion();
-
-        if (cmd.hasOption("verbose"))
-            setVerbose();
+        if (cmd.hasOption("version")) {
+            printVersion(printWriter);
+            throw new SpecialOptionException();
+        }
     }
 
-    /**
-     * Terminates the execution with the help information.
-     */
-    private void printHelp() {
+    private void printHelp(PrintWriter printWriter) {
         HelpFormatter formatter = new HelpFormatter();
-        formatter.setWidth(80);
         formatter.setOptionComparator(null); // print options in the order they were added
-        String name = Compiler.class.getPackage().getImplementationTitle();
+        String name = getImplementationTitle();
         String header = "Compiles the given zkStrata statement into an intermediate representation of gadgets.";
-        formatter.printHelp(name, header, options, null, true);
-        System.exit(1);
+        formatter.printHelp(printWriter, formatter.getWidth(), name, header, options, formatter.getLeftPadding(),
+                formatter.getDescPadding(), null, true);
+    }
+
+    private void printVersion(PrintWriter printWriter) {
+        String name = getImplementationTitle();
+        String version = getImplementationVersion();
+        printWriter.println(String.format("%s %s", name, version));
+    }
+
+    private String getImplementationTitle() {
+        return Optional.ofNullable(Compiler.class.getPackage().getImplementationTitle()).orElse("zkstratac");
+    }
+
+    private String getImplementationVersion() {
+        return Optional.ofNullable(Compiler.class.getPackage().getImplementationVersion()).orElse("0.0");
     }
 
     /**
-     * Terminates the execution with the version information.
+     * Checks whether the verbose option is set.
+     *
+     * @param cmd {@link CommandLine} object that represents a list of arguments
      */
-    private void printVersion() {
-        String name = Compiler.class.getPackage().getImplementationTitle();
-        String version = Compiler.class.getPackage().getImplementationVersion();
-        System.out.println(String.format("%s %s", name, version));
-        System.exit(1);
-    }
-
-    private void setVerbose() {
-        Configurator.setRootLevel(Level.DEBUG);
+    private void setVerbosity(CommandLine cmd) {
+        if (cmd.hasOption("verbose"))
+            Configurator.setRootLevel(Level.DEBUG);
     }
 
     private String getStatementFile(CommandLine cmd) {
