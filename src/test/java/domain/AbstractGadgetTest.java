0801951a559e426f8ac0093cda4848c6773d5e79
Marc <marc.kloter@bluewin.ch>
2019-12-24 20:10:32 +0100

    Add tests to abstract gadget to cover remaining conditions

--- src/test/java/domain/AbstractGadgetTest.java
+++ src/test/java/domain/AbstractGadgetTest.java
@@ -2,9 +2,16 @@ package domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import zkstrata.codegen.TargetFormat;
 import zkstrata.compiler.Arguments;
+import zkstrata.domain.data.types.Literal;
+import zkstrata.domain.data.types.wrapper.InstanceVariable;
+import zkstrata.domain.data.types.wrapper.Variable;
+import zkstrata.domain.gadgets.AbstractGadget;
+import zkstrata.domain.gadgets.Type;
 import zkstrata.domain.visitor.ASTVisitor;
 import zkstrata.exceptions.CompileTimeException;
+import zkstrata.exceptions.InternalCompilerException;
 import zkstrata.parser.ast.AbstractSyntaxTree;
 import zkstrata.parser.ast.Subject;
 import zkstrata.parser.ast.predicates.BoundsCheck;
@@ -15,7 +22,9 @@ import zkstrata.parser.ast.types.IntegerLiteral;
 import zkstrata.parser.ast.types.StringLiteral;
 import zkstrata.utils.ArgumentsBuilder;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 import static zkstrata.utils.TestHelper.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -68,5 +77,56 @@ public class AbstractGadgetTest {
         CompileTimeException exception = assertThrows(CompileTimeException.class, () -> visitor.visit(ast));
 
         assertTrue(exception.getMessage().toLowerCase().contains("not allowed here"));
+    }
+
+    @Test
+    void Unable_To_Access_Should_Throw() {
+        InvalidGadget invalidGadget = new InvalidGadget();
+        InstanceVariable instanceVariable = createInstanceVariable(new Literal(BigInteger.valueOf(17)));
+
+        InternalCompilerException exception = assertThrows(InternalCompilerException.class, () ->
+                invalidGadget.initFrom(Map.of("unableToAccess", instanceVariable))
+        );
+
+        assertTrue(exception.getMessage().toLowerCase().contains("unable to access"));
+    }
+
+    @Test
+    void Missing_Type_Annotation_Should_Throw() {
+        InvalidGadget invalidGadget = new InvalidGadget();
+        InstanceVariable instanceVariable = createInstanceVariable(new Literal(BigInteger.valueOf(17)));
+
+        InternalCompilerException exception = assertThrows(InternalCompilerException.class, () ->
+                invalidGadget.initFrom(Map.of("missingTypeAnnotation", instanceVariable))
+        );
+
+        assertTrue(exception.getMessage().toLowerCase().contains("is missing @type"));
+    }
+
+    public static class InvalidGadget extends AbstractGadget {
+        @Type({BigInteger.class})
+        private static final Variable unableToAccess = null;
+
+        private Variable missingTypeAnnotation;
+
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public List<TargetFormat> toTargetFormat() {
+            return null;
+        }
+
+        @Override
+        public int getCostEstimate() {
+            return 0;
+        }
     }
 }
