ce1783f8d7f931a58573823dd7c5d1355d8e222c
Marc <marc.kloter@bluewin.ch>
2019-12-25 11:05:39 +0100

    Rename methods

--- src/main/java/zkstrata/utils/InferencesTableBuilder.java
+++ src/main/java/zkstrata/utils/InferencesTableBuilder.java
@@ -13,17 +13,17 @@ public class InferencesTableBuilder {
     private SimpleTable simpleTable = new SimpleTable();
 
     public String buildTable(Set<Inference> inferences) {
-        createHeader();
+        appendHeader();
 
         for (Inference inference : inferences)
-            createRow(inference);
+            appendRow(inference);
 
         GridTable gridTable = this.simpleTable.toGrid();
 
         return Util.asString(addBorder(gridTable)).trim();
     }
 
-    private void createHeader() {
+    private void appendHeader() {
         this.simpleTable = this.simpleTable.nextRow()
                 .nextCell(pad("id"))
                 .nextCell(pad("conclusion"))
@@ -31,13 +31,13 @@ public class InferencesTableBuilder {
                 .nextCell(pad("derived from"));
     }
 
-    private void createRow(Inference inference) {
+    private void appendRow(Inference inference) {
         this.simpleTable = this.simpleTable.nextRow();
 
         addIdentifierCell(inference.hashCode());
-        addGadgetCell(inference.getConclusion());
-        addAssumptionsCell(inference.getAssumptions());
-        addDerivedFromCell(inference.getDerivedFrom());
+        appendGadgetCell(inference.getConclusion());
+        appendAssumptionsCell(inference.getAssumptions());
+        appendDerivedFromCell(inference.getDerivedFrom());
     }
 
     private GridTable addBorder(GridTable gridTable) {
@@ -50,36 +50,36 @@ public class InferencesTableBuilder {
 
     private void addIdentifierCell(int identifier) {
         this.simpleTable = this.simpleTable.nextCell();
-        addIdentifierLine(identifier);
+        appendIdentifierLine(identifier);
     }
 
-    private void addIdentifierLine(int identifier) {
+    private void appendIdentifierLine(int identifier) {
         this.simpleTable.addLine(pad(Integer.toHexString(identifier)));
     }
 
-    private void addGadgetCell(Gadget gadget) {
+    private void appendGadgetCell(Gadget gadget) {
         this.simpleTable = this.simpleTable.nextCell();
 
-        addGadgetLines(gadget);
+        appendGadgetLines(gadget);
     }
 
-    private void addGadgetLines(Gadget gadget) {
+    private void appendGadgetLines(Gadget gadget) {
         for (String line : gadget.getVerboseInformation().split(System.lineSeparator()))
             this.simpleTable = this.simpleTable.addLine(pad(line));
     }
 
-    private void addAssumptionsCell(Set<Gadget> assumptions) {
+    private void appendAssumptionsCell(Set<Gadget> assumptions) {
         this.simpleTable = this.simpleTable.nextCell();
 
         for (Gadget assumption : assumptions)
-            addGadgetLines(assumption);
+            appendGadgetLines(assumption);
     }
 
-    private void addDerivedFromCell(Set<Inference> derivedFrom) {
+    private void appendDerivedFromCell(Set<Inference> derivedFrom) {
         this.simpleTable = this.simpleTable.nextCell();
 
         for (Inference axiom : derivedFrom)
-            addIdentifierLine(axiom.hashCode());
+            appendIdentifierLine(axiom.hashCode());
     }
 
     private String pad(String string) {
