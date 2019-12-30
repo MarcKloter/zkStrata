3264c89fcb37a249d8f8eeb5d278c231765d7c67
Marc <marc.kloter@bluewin.ch>
2019-12-30 12:43:14 +0100

    Revert target refactoring

--- src/main/java/zkstrata/exceptions/Position.java
+++ src/main/java/zkstrata/exceptions/Position.java
@@ -3,18 +3,18 @@ package zkstrata.exceptions;
 import java.io.Serializable;
 
 public abstract class Position implements Serializable {
-    private String statement;
+    private String target;
     private int line;
     private int charPositionInLine;
 
-    public Position(String statement, int line, int charPositionInLine) {
-        this.statement = statement;
+    public Position(String target, int line, int charPositionInLine) {
+        this.target = target;
         this.line = line;
         this.charPositionInLine = charPositionInLine;
     }
 
-    public String getStatement() {
-        return statement;
+    public String getTarget() {
+        return target;
     }
 
     public int getLine() {
@@ -33,14 +33,20 @@ public abstract class Position implements Serializable {
 
     public static class Absolute extends Position {
         private String source;
+        private String statement;
 
         public Absolute(String source, String statement, Position position) {
-            super(statement, position.getLine(), position.getPosition());
+            super(position.getTarget(), position.getLine(), position.getPosition());
             this.source = source;
+            this.statement = statement;
         }
 
         public String getSource() {
             return source;
+        }
+
+        public String getStatement() {
+            return statement;
         }
 
         @Override
