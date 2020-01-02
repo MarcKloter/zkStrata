f129f05679469f3c3f43923e23732b0106a8b787
Marc <marc.kloter@bluewin.ch>
2020-01-02 17:11:49 +0100

    Add generic tests for abstract mapper

---
+++ src/test/java/domain/AbstractMapperTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+import zkstrata.domain.gadgets.mapper.LessThanMapper;
+import zkstrata.exceptions.InternalCompilerException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AbstractMapperTest {
+    @Test
+    void Assert_Equals_Throws() {
+        assertThrows(InternalCompilerException.class, () -> new LessThanMapper().equals(null));
+    }
+
+    @Test
+    void Assert_HashCode_Throws() {
+        assertThrows(InternalCompilerException.class, () -> new LessThanMapper().hashCode());
+    }
+
+    @Test
+    void Assert_ToTargetFormat_Throws() {
+        assertThrows(InternalCompilerException.class, () -> new LessThanMapper().toTargetFormat());
+    }
+
+    @Test
+    void Assert_GetCostEstimate_Throws() {
+        assertThrows(InternalCompilerException.class, () -> new LessThanMapper().getCostEstimate());
+    }
+}
